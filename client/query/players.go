7a064650e8c9c8aceb187b1bef04766ec518c33c
satetsu888 <satetsu888@gmail.com>
2022-09-19 22:08:04 +0900

    refactoring around response string regex

--- client/query/players.go
+++ client/query/players.go
@@ -10,29 +10,27 @@ import (
 )
 
 func FetchPlayerPosition(c *minecraft.Client, playerName string) (player.Position, error) {
-	baseReg := regexp.MustCompile(`(?P<playerName>\S+) has the following entity data: (?P<data>.+)`)
+	reg := regexp.MustCompile(`(?P<playerName>\S+) has the following entity data: \[(?P<x>\S+)d, (?P<y>\S+)d, (?P<z>\S+)d\]`)
 
 	res, err := c.SendCommand("data get entity @p[name=" + playerName + "] Pos")
 	if err != nil {
 		return player.Position{}, fmt.Errorf("failed to fetch player Pos: %w", err)
 	}
-	result := baseReg.FindAllStringSubmatch(res.Body, -1)
-	data := result[0][2]
 
-	positionReg := regexp.MustCompile(`\[(?P<x>\S+)d, (?P<y>\S+)d, (?P<z>\S+)d\]`)
-	result = positionReg.FindAllStringSubmatch(data, -1)
-	x, err := strconv.ParseFloat(result[0][1], 64)
+	result := reg.FindAllStringSubmatch(res.Body, -1)
+	x, err := strconv.ParseFloat(result[0][2], 64)
 	if err != nil {
 		return player.Position{}, err
 	}
-	y, err := strconv.ParseFloat(result[0][2], 64)
+	y, err := strconv.ParseFloat(result[0][3], 64)
 	if err != nil {
 		return player.Position{}, err
 	}
-	z, err := strconv.ParseFloat(result[0][3], 64)
+	z, err := strconv.ParseFloat(result[0][4], 64)
 	if err != nil {
 		return player.Position{}, err
 	}
+
 	return player.Position{
 		X: x,
 		Y: y,
@@ -41,22 +39,18 @@ func FetchPlayerPosition(c *minecraft.Client, playerName string) (player.Positio
 }
 
 func FetchPlayerRotation(c *minecraft.Client, playerName string) (player.Rotation, error) {
-	baseReg := regexp.MustCompile(`(?P<playerName>\S+) has the following entity data: (?P<data>.+)`)
+	reg := regexp.MustCompile(`(?P<playerName>\S+) has the following entity data: \[(?P<yaw>\S+)f, (?P<pitch>\S+)f\]`)
 
 	res, err := c.SendCommand("data get entity @p[name=" + playerName + "] Rotation")
 	if err != nil {
 		return player.Rotation{}, fmt.Errorf("failed to fetch player Rotation: %w", err)
 	}
-	result := baseReg.FindAllStringSubmatch(res.Body, -1)
-	data := result[0][2]
-
-	rotationReg := regexp.MustCompile(`\[(?P<yaw>\S+)f, (?P<pitch>\S+)f\]`)
-	result = rotationReg.FindAllStringSubmatch(data, -1)
-	yaw, err := strconv.ParseFloat(result[0][1], 64)
+	result := reg.FindAllStringSubmatch(res.Body, -1)
+	yaw, err := strconv.ParseFloat(result[0][2], 64)
 	if err != nil {
 		return player.Rotation{}, err
 	}
-	pitch, err := strconv.ParseFloat(result[0][2], 64)
+	pitch, err := strconv.ParseFloat(result[0][3], 64)
 	if err != nil {
 		return player.Rotation{}, err
 	}
@@ -67,16 +61,12 @@ func FetchPlayerRotation(c *minecraft.Client, playerName string) (player.Rotatio
 }
 
 func FetchPlayerDimention(c *minecraft.Client, playerName string) (player.Dimension, error) {
-	baseReg := regexp.MustCompile(`(?P<playerName>\S+) has the following entity data: (?P<data>.+)`)
+	reg := regexp.MustCompile(`(?P<playerName>\S+) has the following entity data: (?P<dimension>\S+)`)
 
 	res, err := c.SendCommand("data get entity @p[name=" + playerName + "] Dimension")
 	if err != nil {
 		return player.Dimension(""), fmt.Errorf("failed to fetch player Dimension: %w", err)
 	}
-	result := baseReg.FindAllStringSubmatch(res.Body, -1)
-	data := result[0][2]
-
-	dimensionReg := regexp.MustCompile(`(?P<dimension>\S+)`)
-	result = dimensionReg.FindAllStringSubmatch(data, -1)
-	return player.Dimension(result[0][1]), nil
+	result := reg.FindAllStringSubmatch(res.Body, -1)
+	return player.Dimension(result[0][2]), nil
 }
